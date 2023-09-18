63b5fe6c2452ca239a2413dc736817d7f9657cfb
Eli Roden <elijahleeroden@gmail.com>
2023-09-18 00:55:05 -0500

    cast database name to string

--- Chess/chessWebsite/settings.py
+++ Chess/chessWebsite/settings.py
@@ -92,7 +92,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
