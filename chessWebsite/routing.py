492d379cb2e30de635cb248039f80e5758833b5d
Eli Roden <elijahleeroden@gmail.com>
2023-09-20 03:50:29 -0500

    refactor

--- chessWebsite/routing.py
+++ chessWebsite/routing.py
@@ -1,12 +1,13 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.urls import path, re_path
+from django.urls import re_path
 
-from . import chessApp
+from chessApp.consumers import ChessConsumer
 
 application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(chessApp.routing.websocket_urlpatterns))
+            AuthMiddlewareStack(
+                URLRouter([re_path(r"ws/game/(?P<game_id>\w+)/$", ChessConsumer)]))
         ),
 })
