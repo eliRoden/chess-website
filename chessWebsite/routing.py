935b3e530b226f5cb1a9a4ee231433e618e7765e
Eli Roden <elijahleeroden@gmail.com>
2023-09-21 02:25:02 -0500

    update

--- chessWebsite/routing.py
+++ chessWebsite/routing.py
@@ -2,7 +2,6 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path, re_path
-from django.core.asgi import get_asgi_application
 from chessApp.consumers import ChessConsumer
 
 application = ProtocolTypeRouter({
