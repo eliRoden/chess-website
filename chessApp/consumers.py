be6e4a615914d1557f99aae6190f63b3722f479d
Eli Roden <elijahleeroden@gmail.com>
2023-09-21 01:05:44 -0500

    update

--- chessApp/consumers.py
+++ chessApp/consumers.py
@@ -9,6 +9,7 @@ class ChessConsumer(WebsocketConsumer):
         self.game_id = self.scope["url_route"]["kwargs"]["game_id"]
         self.game_group_name = "chat_%s" % self.game_id
 
+        
         async_to_sync(self.channel_layer.group_add)(
             self.game_group_name, self.channel_name
         )
