c6cc3b4fdb0b10754a4a0d3439626cb452ebcd2a
Eli Roden <elijahleeroden@gmail.com>
2023-09-20 23:33:28 -0500

    update websocket config

--- chessWebsite/routing.py
+++ chessWebsite/routing.py
@@ -1,13 +1,16 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.urls import re_path
+from django.urls import path, re_path
 
 from chessApp.consumers import ChessConsumer
 
 application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(
-                URLRouter([re_path(r"ws/game/(?P<game_id>\w+)/$", ChessConsumer)]))
+                URLRouter([
+                    path('game/<game_id>/', ChessConsumer),
+                ])
+            )
         ),
 })
