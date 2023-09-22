d910ba0dfe3519c09b90fe11ce683f6e20230464
Eli Roden <elijahleeroden@gmail.com>
2023-09-22 00:29:59 -0500

    change database to PostgreSQL

--- chessWebsite/settings.py
+++ chessWebsite/settings.py
@@ -87,11 +87,14 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
