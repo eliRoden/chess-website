8bcc2a61e5378772ce6b181b33391557b7dd1921
Eli Roden <elijahleeroden@gmail.com>
2023-09-23 18:09:15 -0500

    update

--- chessWebsite/routing.py
+++ chessWebsite/routing.py
@@ -3,7 +3,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path, re_path
 
 from chessApp.consumers import ChessConsumer
-print('in asgi.py')
+#print('in asgi.py')
 
 application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
