8506a3e975a5753bedabe012a7c44698ffa9ee19
Eli Roden <elijahleeroden@gmail.com>
2023-09-24 14:52:17 -0500

    update consumer

--- chessWebsite/settings.py
+++ chessWebsite/settings.py
@@ -66,7 +66,10 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
 ]
 
+CORS_ALLOWED_ORIGINS = ['*']
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
